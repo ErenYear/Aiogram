ca10cfad0ca62834fbdc6a0b8bac03e7120691e9
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-24 12:42:43 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,8 @@
 from aiogram.types import Message
+from aiogram.filters import Command
 
 from main import dp
 
-
-@dp.message(commands="start")
+@dp.message(Command("start"))
 async def user_start(message: Message):
     await message.reply("Welcome!")
