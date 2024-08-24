396b78a5f3531220152cf61d6f414d03b4e0b5e2
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-24 13:29:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 from config import TOKEN
 import setup
 
-bot = Bot(token=(TOKEN))
+bot = Bot(
+    token=(TOKEN),
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+)
 dp = Dispatcher()
 
-if __name__ == "__main__":
-    dp.run_polling(bot)
+async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
