b48d35ede3892809632e05a850a694c320977300
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-24 13:47:15 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,6 +3,7 @@ from aiogram.filters import Command
 
 from main import dp
 
-@dp.message(Command("lund"))
+@dp.message(Command(commands=["lund"]))
 async def user_start(message: Message):
     await message.reply("Welcome!")
+    
