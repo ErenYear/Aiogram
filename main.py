6e6dbe75928b2dcafed7d52686009bcbd1cea9d9
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-24 14:18:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
 from config import TOKEN
-import setup
+from handlers.user.start import lunduser
 
 bot = Bot(
     token=TOKEN,
     default=DefaultBotProperties(parse_mode=ParseMode.HTML),
 )
 dp = Dispatcher()
+dp.include_router(lunduser)
 
 async def on_startup():
     print("Bot has started successfully.")
