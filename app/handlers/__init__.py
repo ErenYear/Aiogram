0aa3a93e285b7ee969d47a661678ecf543fdbcf0
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-24 17:12:40 +0530

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,1 +1,15 @@
-lund
+from aiogram import Dispatcher
+from aiogram.types import ErrorEvent
+
+from .logging import logger
+
+from .user import help_router as help, start_router as start
+
+
+def setup_handlers(dp: Dispatcher) -> None:
+    @dp.error()
+    async def _error(event: ErrorEvent):
+        logger.exception(event.exception)
+
+    dp.include_routers(start, help)
+  
