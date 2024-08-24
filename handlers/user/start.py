40dbed74580be57f83197ad5359d2eb61f58890c
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-24 14:38:26 +0530

    Update start.py

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -1,10 +1,8 @@
 from aiogram.types import Message
 from aiogram.filters import Command
-from aiogram import Router
+from handlers import app
 
-lunduser = Router()
-
-@lunduser.message(Command("lund"))
+@app.message(Command("lund"))
 async def user_start(message: Message):
     await message.reply("Welcome!")
     
