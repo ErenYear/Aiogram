4b8830cbd75d38d211c0f305e25531a7b9ab44f9
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-24 14:01:58 +0530

    Delete handlers/start.py

--- handlers/start.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import Message
-from aiogram.filters import Command
-
-from main import dp
-
-@dp.message(Command(commands=["lund"]))
-async def user_start(message: Message):
-    await message.reply("Welcome!")
-    
