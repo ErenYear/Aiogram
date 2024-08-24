516c57226f6726cf9cdec2ea5264555859aefe72
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-24 14:28:16 +0530

    Create help.py

---
+++ handlers/user/help.py
@@ -0,0 +1,15 @@
+from aiogram.types import Message
+from aiogram.filters import Command
+from .import lunduser
+    
+
+@lunduser.message(Command("help"))
+async def help_command(message: Message):
+    help_text = (
+        "Here are some commands you can use:\n"
+        "/start - Start interacting with the bot\n"
+        "/help - Get a list of available commands\n"
+        "/info - Get information about this bot\n"
+        "/lund - A fun command to welcome you"
+    )
+    await message.reply(help_text)
